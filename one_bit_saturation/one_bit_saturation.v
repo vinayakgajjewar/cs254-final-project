1f5588113c8bfa8af630e17a9f558d4666a32a00
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2023-11-19 14:12:00 -0800

    change from 2-bit to 1-bit

--- one_bit_saturation/one_bit_saturation.v
+++ one_bit_saturation/one_bit_saturation.v
@@ -2,23 +2,21 @@ module one_bit_saturation (
     input wire clk,          // Clock input
     input wire rst,          // Reset input
     input wire branch_taken,
-    input wire [1:0] counter,
+    input wire counter,       // 1-bit counter
     output reg predict
 );
 
     // Saturation counter states
-    parameter IDLE = 2'b00;
-    parameter WEAKLY_NOT_TAKEN = 2'b01;
-    parameter WEAKLY_TAKEN = 2'b10;
-    parameter STRONGLY_TAKEN = 2'b11;
+    parameter NOT_TAKEN = 1'b0;
+    parameter TAKEN = 1'b1;
 
     // Internal counter register
-    reg [1:0] counter_reg;
+    reg counter_reg;
 
     // Synchronous reset
     always @(posedge clk or posedge rst) begin
         if (rst)
-            counter_reg <= IDLE;
+            counter_reg <= NOT_TAKEN;
         else
             counter_reg <= counter;
     end
@@ -26,21 +24,17 @@ module one_bit_saturation (
     // Update counter based on branch outcome
     always @(posedge clk) begin
         if (branch_taken) begin
-            case (counter_reg)
-                IDLE: counter_reg <= WEAKLY_NOT_TAKEN;
-                WEAKLY_NOT_TAKEN: counter_reg <= WEAKLY_TAKEN;
-                WEAKLY_TAKEN: counter_reg <= STRONGLY_TAKEN;
-                STRONGLY_TAKEN: counter_reg <= STRONGLY_TAKEN;
-            endcase
+            if (counter_reg == NOT_TAKEN)
+                counter_reg <= TAKEN;
+        end else begin
+            if (counter_reg == TAKEN)
+                counter_reg <= NOT_TAKEN;
         end
     end
 
     // Predict branch outcome based on counter
     always @* begin
-        case (counter_reg)
-            IDLE, WEAKLY_NOT_TAKEN: predict = 1'b0;
-            WEAKLY_TAKEN, STRONGLY_TAKEN: predict = 1'b1;
-        endcase
+        predict = counter_reg;
     end
 
 endmodule
