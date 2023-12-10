85837a9966810503c940f142e3f7fa7587cee8b4
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2023-12-10 12:17:36 -0800

    Rename gshare testbench file for consistency

--- gshare/testbench.py
+++ gshare/testbench.py
@@ -1,3 +1,5 @@
+# Testbench for the gshare branch predictor.
+
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge
@@ -14,36 +16,40 @@ async def gshare(dut):
 
     # Create an instance of our evaluator.
     evaluator = Evaluator()
-    evaluator.load_trace()
+    evaluator.load_trace("../traces/trace_01")
 
     # generate a clock with a 10 ns period
-    cocotb.start_soon(Clock(dut.clock, 10, 'ns').start())
+    cocotb.start_soon(Clock(dut.clk, 10, 'ns').start())
 
     # reset the module and wait 2 rising edges before we release the reset
-    dut.reset.value = 1
+    dut.rst.value = 1
     for _ in range(2):
-        await RisingEdge(dut.clock)
-    dut.reset.value = 0
+        await RisingEdge(dut.clk)
+    dut.rst.value = 0
 
     # Get the first branch record from the trace.
     next_branch = evaluator.get_next_branch_record()
 
     # Run our evaluation loop 10 times for now.
     for i in range(10):
-        await RisingEdge(dut.clock)
+        await RisingEdge(dut.clk)
 
         print(f"instruction pc: {next_branch.pc}")
 
-        print(f"state: {dut.counter_reg.value}")
+        # Tell gshare what the PC of the next branch is. We strip the "0x" from
+        # the string and convert it to a binary buffer of bytes.
+        dut.pc.value.buff = bytes.fromhex(next_branch.pc[2:])
+
         print(f"prediction: {dut.predict.value}")
+        print(f"type(dut.predict.value)={type(dut.predict.value)}")
 
         # Report our current prediction and get the actual result.
         if (evaluator.predict_branch(dut.predict.value)):
             print("Yay! Correctly predicted")
-            dut.branch_taken.value = dut.predict.value
+            dut.taken.value = dut.predict.value
         else:
             print("Aw :( Incorrectly predicted")
-            dut.branch_taken.value = not dut.predict.value
+            dut.taken.value = not dut.predict.value
 
         # Get the next branch
         next_branch = evaluator.get_next_branch_record()
